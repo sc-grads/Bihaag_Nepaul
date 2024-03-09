6791278ad77932ec3ef8f07b9614778f96c4b14d
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-09 20:18:41 +0200

    changed sql for database table creation

--- GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
+++ GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
@@ -1,8 +1,8 @@
-Use UserDatabase;
+Use UserDatabase
 
 CREATE TABLE users (
     user_id INT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE,
-    birthdate DATE,
+    birthdate DATE
 );
