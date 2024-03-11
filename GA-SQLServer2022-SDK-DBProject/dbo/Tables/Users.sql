c0013c7898c3bc392d54e46f8872193315ad30ce
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-11 15:19:47 +0200

    q1-Insert

--- GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
+++ GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
@@ -1,5 +1,4 @@
 INSERT INTO [dbo].[Users] ([FirstName], [LastName], [Email])
 VALUES 
-    ('John', 'Doe', 'john.doe@example.com'),
-    ('Jane', 'Smith', 'jane.smith@example.com'),
-    ('Alice', 'Johnson', 'alice.johnson@example.com');
+    ('Ndivu', 'Bro', 'Ndivu.bro@example.com');
+
