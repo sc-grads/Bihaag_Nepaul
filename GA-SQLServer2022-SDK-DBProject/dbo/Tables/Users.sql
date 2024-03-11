6c941aab04a92751aa4bed1069c290f0289ddea5
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-11 18:20:12 +0200

    using employeehub

--- GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
+++ GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
@@ -1,1 +1,5 @@
-EXEC InsertUserName @FirstName = 'Bob', @LastName = 'TheBUILDER';
+-- Insert a new employee into [EmployeeHub].[dbo].[employees]
+USE EmployeeHub
+
+INSERT INTO [EmployeeHub].[dbo].[employees] (FirstName, LastName, Email, Department, Position, Salary)
+VALUES ('John', 'Doe', 'john.doe@example.com', 'IT', 'Software Developer', 80000);
