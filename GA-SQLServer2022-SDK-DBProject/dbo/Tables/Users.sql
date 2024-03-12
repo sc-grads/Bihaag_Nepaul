77af5acaf4c3dee6761e514e1c79dd66c40d7823
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-12 16:53:52 +0200

    Uploading data to Employees table

--- GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
+++ GA-SQLServer2022-SDK-DBProject/dbo/Tables/Users.sql
@@ -1,4 +1,4 @@
 USE EmployeeHub;
 
 INSERT INTO [EmployeeHub].[dbo].[employees] (EmployeeID, FirstName, LastName, Email, Department, Position, Salary)
-VALUES (11, 'John', 'Doe', 'john.doe@example.com', 'IT', 'Software Developer', 80000);
+VALUES (11, 'Mike', 'Doe', 'Kitty.doe@example.com', 'HR', 'Software stealer', 80000);
