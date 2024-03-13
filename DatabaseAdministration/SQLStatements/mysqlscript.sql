85f03e861d0b9534179dd4d8f0ac610b65e08b4e
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-13 12:27:07 +0200

    Update mysqlscript.sql

--- DatabaseAdministration/SQLStatements/mysqlscript.sql
+++ DatabaseAdministration/SQLStatements/mysqlscript.sql
@@ -1,23 +1,24 @@
-CREATE DATABASE NewDatabase;
+--CREATE DATABASE NewDatabase;
 
-GO
+--GO
 
 USE NewDatabase;
 
 
-CREATE TABLE [dbo].[employees](
-[EmployeeID] [int] NOT NULL,
-[FirstName] [varchar](50) NULL,
-[LastName] [varchar](50) NULL,
-);
+--CREATE TABLE [dbo].[employees](
+--[EmployeeID] [int] NOT NULL,
+--[FirstName] [varchar](50) NULL,
+--[LastName] [varchar](50) NULL,
+--);
 
 Insert into [dbo].[employees] (EmployeeID,FirstName,LastName)
-VALUES (1,'Bihaag','Nepaul');
+VALUES (2,'Bongani','Mondlane');
+
 
 
 SELECT * FROM employees;
   
-SELECT name
-FROM sys.databases;
+--SELECT name
+--FROM sys.databases;
 
 
