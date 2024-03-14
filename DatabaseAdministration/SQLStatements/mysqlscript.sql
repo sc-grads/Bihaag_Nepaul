479215d4abace26210dab648c394696ee10b6163
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-14 15:06:26 +0200

    Update mysqlscript.sql

--- DatabaseAdministration/SQLStatements/mysqlscript.sql
+++ DatabaseAdministration/SQLStatements/mysqlscript.sql
@@ -14,26 +14,7 @@ Select * from [dbo].[GenderAvg];
 --Insert into [dbo].[employees] (EmployeeID,FirstName,LastName)
 --VALUES (2,'Bongani','Mondlane');
 
---USE NewDatabase;
---GO
 
---CREATE PROCEDURE InsertEmployee
-  --  @EmployeeID INT,
-    --@FirstName VARCHAR(50),
-    --@LastName VARCHAR(50)
---AS
---BEGIN
-    --INSERT INTO [dbo].[employees] (EmployeeID, FirstName, LastName)
-    --VALUES (@EmployeeID, @FirstName, @LastName);
---END;
-
---EXEC InsertEmployee @EmployeeID = 5, @FirstName = 'Bongani', @LastName = 'Mondlane';
-
-
-----SELECT * FROM employees;
-  
---SELECT name
---FROM sys.databases;
 
 /*INSERT INTO [EmployeeHub].[dbo].[EmployeeData] (
     [BusinessEntityID],
