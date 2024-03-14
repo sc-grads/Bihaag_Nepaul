9abf26a5bb05fee88b5d43358d5b8021f6cc2dba
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-14 13:10:41 +0200

    Create email.sql

---
+++ DatabaseAdministration/SQLStatements/email.sql
@@ -0,0 +1,5 @@
+EXEC msdb.dbo.sp_send_dbmail
+ @recipients = 'sqlreportsbn@gmail.com',
+ @subject = 'Average Salary per Gender',  
+ @body = 'Company Employee Average Salary',
+ @query = 'USE EmployeeHub SELECT * FROM [dbo].[GenderAvg];';
