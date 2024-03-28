9e2e0ef8aa5438e4fb63a94810578d2f6f646abb
Bihaag Nepaul-SC <159785050+BN-Sambe@users.noreply.github.com>
2024-03-28 16:50:41 +0200

    Update PublishReport.ps1

--- PublishReport.ps1
+++ PublishReport.ps1
@@ -1,6 +1,6 @@
 param (
     [string] $ReportPath = "DatabaseAdministration/SSRS/Reports/DepReportNew.rdl",
-    [string] $ReportServerUri = "http://localhost/ReportServer",
+    [string] $ReportServerUri = "https://c840-105-226-88-142.ngrok-free.app/ReportServer",
     [string] $TargetFolder = "/SharedReports"
 )
 
