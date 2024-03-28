2e8769c336646168e881f798bfb88f323b9e2ead
BN-Sambe <bihaag.nepaul@sambeconsulting.com>
2024-03-28 14:39:22 +0200

    completed exercise

--- DatabaseAdministration/Exercises/AdventureWorksPart1.sql
+++ DatabaseAdministration/Exercises/AdventureWorksPart1.sql
@@ -56,3 +56,176 @@ inner join person.PersonPhone as phoneno
 on person.BusinessEntityID = phoneno.BusinessEntityID
 where lastname LIKE 'L%'  
 ORDER BY lastname, firstname;
+
+--Solution 12 **
+select * from [Sales].[SalesOrderHeader]
+
+/*
+Syntax:
+
+SELECT column, aggregate_function(column)
+
+FROM table
+
+GROUP BY ROLLUP (column);*/
+
+SELECT salespersonid, customerid, SUM(SubTotal) AS sum_total from [Sales].[SalesOrderHeader] WHERE SalesPersonID IS NOT NULL
+GROUP BY ROLLUP (salespersonid, customerid);
+
+--Solution 13**
+select locationid, shelf, SUM(quantity) as totalquantity from [Production].[ProductInventory]
+Group by cube (locationid, shelf);
+
+--tried also using rollup
+
+--Solution 14**
+
+select locationid, shelf, SUM(Quantity) as totalquantity from [Production].[ProductInventory]
+Group by grouping sets (rollup (locationId,shelf), CUBE (locationid,shelf));
+
+--Solution 15**
+
+select locationid, SUM(Quantity) as TotalQuantity 
+from [Production].[ProductInventory] GROUP BY GROUPING SETS (locationid);
+
+--Solution 16
+ select * from [Person].[BusinessEntityAddress]
+select * from [Person].[Address]
+
+
+select pa.city, count(ba.addressid) as noofemployees
+from [Person].[BusinessEntityAddress] ba
+join [Person].[Address] pa
+on ba.addressid = pa.addressid
+group by city
+order by city asc;
+
+--Solution 17
+select * from [Sales].[SalesOrderHeader]
+
+select year(orderdate) as year, sum(totaldue) as orderamount
+from [Sales].[SalesOrderHeader]
+group by year(orderdate)
+order by year asc;
+
+--Solution 18
+select year(orderdate) as year, sum(totaldue) as orderamount
+from [Sales].[SalesOrderHeader]
+group by year(orderdate)
+having year(orderdate) <=2016
+order by year asc;
+
+--Solution 19
+select * from [Person].[ContactType]
+
+select contacttypeid, name from [Person].[ContactType] WHERE name LIKE '%Manager%'
+Order by name desc
+
+--Solution 20
+select * from [Person].[BusinessEntityContact]
+select * from [Person].[ContactType]
+select * from [Person].[Person]
+
+
+
+select psn.BusinessEntityID, psn.firstname, psn.lastname
+
+from [Person].[BusinessEntityContact] as bec
+
+inner join [Person].[ContactType] as ct
+on ct.ContactTypeID = bec.ContactTypeID
+
+inner join [Person].[Person] as psn
+on psn.BusinessEntityID = bec.PersonID
+
+where ct.Name = 'Purchasing Manager'
+
+order by lastname, firstname ASC;
+
+--Solution 21
+
+Select * from [Person].[Person]
+Select * from [Person].[Address]
+Select * from [Sales].[SalesPerson]
+
+
+select ROW_NUMBER() Over (Partition by PostalCode Order by salesytd desc) as "Row Number",
+p.lastname, sls.salesytd, ad.postalcode
+
+from [Sales].[SalesPerson] as sls
+
+join [Person].[Person] as p
+on sls.BusinessEntityID = p.BusinessEntityID
+
+join [Person].[Address] as ad
+on ad.AddressID = p.BusinessEntityID
+
+where TerritoryID IS NOT NULL 
+	and SalesYTD <> 0
+Order by PostalCode;
+
+--Solution 22
+
+select * from [Person].[BusinessEntityContact]
+select * from [Person].[ContactType]
+
+
+select ct.contacttypeid, ct.name as ctypename, COUNT(*) as nocontacts
+
+from [Person].[BusinessEntityContact] as bec
+join  [Person].[ContactType] as ct 
+on ct.ContactTypeID = bec.ContactTypeID
+
+group by ct.ContactTypeID,ct.Name
+having count(*) >= 100
+order by count(*) desc;
+
+
+--Solution 23
+select * from [HumanResources].[EmployeePayHistory]
+select * from [Person].[Person]
+
+select cast(hr.ratechangedate as date) as fromdate, CONCAT(lastname, ',', firstname, ' ',middleName) as NameinFull,
+(40*hr.rate) as salaryinaweek
+
+from [Person].[Person] as p
+join [HumanResources].[EmployeePayHistory] as hr
+on hr.BusinessEntityID = p.BusinessEntityID
+order by NameinFull;
+
+
+--Solution 24
+select cast(hr.ratechangedate as date) as fromdate, CONCAT(lastname, ',', firstname, ' ',middleName) as NameinFull,
+(40*hr.rate) as salaryinaweek
+
+from [Person].[Person] as p
+join [HumanResources].[EmployeePayHistory] as hr
+on hr.BusinessEntityID = p.BusinessEntityID
+
+where hr.RateChangeDate = (select MAX(ratechangedate) from [HumanResources].[EmployeePayHistory] where BusinessEntityID = hr.BusinessEntityID)
+
+
+order by NameinFull;
+
+--Solution 25
+select * from [Sales].[SalesOrderDetail];
+
+select SalesOrderID,ProductID,OrderQty,
+
+SUM(OrderQty) over (PARTITION BY Salesorderid) as "TotalQuantity",
+
+AVG(OrderQty) over (PARTITION BY salesorderid) as "No of Orders",
+
+count(OrderQty) over (PARTITION BY salesorderid) as "No of Orders",
+
+min(OrderQty) over (PARTITION BY salesorderid) as "Min Quantity",
+
+max(OrderQty) over (PARTITION BY salesorderid) as "Max Quantity"
+from [Sales].[SalesOrderDetail]
+
+where SalesOrderID IN (43659,43664);
+
+
+
+
+
