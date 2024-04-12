82a098fb9da20dbb54282040a540491b33b9ecb2
BN-Sambe <bihaag.nepaul@sambeconsulting.com>
2024-04-12 10:26:19 +0200

    A bunch of stuff including updated SSISdeployment ps1 script

---
+++ SSISDeployment.ps1
@@ -0,0 +1,36 @@
+# Variables
+$SSISNamespace = "Microsoft.SqlServer.Management.IntegrationServices"
+$TargetServerName = "DESKTOP-6JDNS4N"
+$TargetFolderName = "SSIS-DataFlowAuditing"
+$ProjectFilePath = "C:\Users\Bihaag Nepaul\Desktop\GitHub Reponew\Bihaag_Nepaul\DatabaseAdministration\SSIS\Ispac\SSIS-DataFlowAuditing.ispac"
+
+$ProjectName = "SSIS-DataFlowAuditing"
+
+
+# Load the IntegrationServices assembly
+$loadStatus = [System.Reflection.Assembly]::Load("Microsoft.SQLServer.Management.IntegrationServices, "+
+    "Version=16.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91, processorArchitecture=MSIL")
+
+# Create a connection to the server
+$sqlConnectionString = "Data Source=" + $TargetServerName + ";Initial Catalog=master;Integrated Security=SSPI;"
+$sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString
+
+
+# Create the Integration Services object
+$integrationServices = New-Object $SSISNamespace".IntegrationServices" $sqlConnection
+
+# Get the Integration Services catalog
+$catalog = $integrationServices.Catalogs["SSISDB"]
+
+# Create the target folder
+$folder = New-Object $SSISNamespace".CatalogFolder" ($catalog, $TargetFolderName,
+    "Folder description")
+$folder.Create()
+
+Write-Host "Deploying " $ProjectName " project ..."
+
+# Read the project file and deploy it
+[byte[]] $projectFile = [System.IO.File]::ReadAllBytes($ProjectFilePath)
+$folder.DeployProject($ProjectName, $projectFile)
+
+Write-Host "Done."
