84967a8e0fd10b193412bbe216a666ea5d0e3ea8
BN-Sambe <bihaag.nepaul@sambeconsulting.com>
2024-04-12 11:08:01 +0200

    new ssis pwsh script

--- SSISDeployment.ps1
+++ SSISDeployment.ps1
@@ -1,36 +1,46 @@
+# Import the SqlServer module
+Import-Module SqlServer -Force
+
+# Get the directory where SqlServer module is installed
+$modulePath = (Get-Module -Name SqlServer).Path
+$assemblyPath = Join-Path -Path $modulePath -ChildPath "Microsoft.SqlServer.Management.IntegrationServices.dll"
+
+# Load the assembly
+$loadStatus = [System.Reflection.Assembly]::LoadFrom($assemblyPath)
+
+# Check if assembly loaded successfully
+if ($loadStatus) {
+    Write-Host "Assembly loaded successfully."
+} else {
+    Write-Host "Failed to load assembly."
+    exit 1  # Exit with an error code if assembly loading fails
+}
+
 # Variables
 $SSISNamespace = "Microsoft.SqlServer.Management.IntegrationServices"
-$TargetServerName = "DESKTOP-6JDNS4N"
+$TargetServerName = "0.tcp.eu.ngrok.io,12490"
 $TargetFolderName = "SSIS-DataFlowAuditing"
 $ProjectFilePath = "DatabaseAdministration/SSIS/Ispac/SSIS-DataFlowAuditing.ispac"
-
 $ProjectName = "SSIS-DataFlowAuditing"
 
-
-# Load the IntegrationServices assembly
-$loadStatus = [System.Reflection.Assembly]::Load("Microsoft.SQLServer.Management.IntegrationServices, "+
-    "Version=16.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91, processorArchitecture=MSIL")
-
 # Create a connection to the server
-$sqlConnectionString = "Data Source=" + $TargetServerName + ";Initial Catalog=master;Integrated Security=SSPI;"
+$sqlConnectionString = "Data Source=$TargetServerName;Initial Catalog=master;Integrated Security=SSPI;"
 $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString
 
-
 # Create the Integration Services object
-$integrationServices = New-Object $SSISNamespace".IntegrationServices" $sqlConnection
+$integrationServices = New-Object "$SSISNamespace.IntegrationServices" $sqlConnection
 
 # Get the Integration Services catalog
 $catalog = $integrationServices.Catalogs["SSISDB"]
 
 # Create the target folder
-$folder = New-Object $SSISNamespace".CatalogFolder" ($catalog, $TargetFolderName,
-    "Folder description")
+$folder = New-Object "$SSISNamespace.CatalogFolder" ($catalog, $TargetFolderName, "Folder description")
 $folder.Create()
 
-Write-Host "Deploying " $ProjectName " project ..."
+Write-Host "Deploying $ProjectName project ..."
 
 # Read the project file and deploy it
-[byte[]] $projectFile = [System.IO.File]::ReadAllBytes($ProjectFilePath)
+$projectFile = [System.IO.File]::ReadAllBytes($ProjectFilePath)
 $folder.DeployProject($ProjectName, $projectFile)
 
 Write-Host "Done."
